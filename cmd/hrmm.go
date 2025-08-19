6850e69573a64b93a44d0978f51d5fcb2c98e2b7
Matthew McPherrin <git@mcpherrin.ca>
2025-08-19 19:02:49 -0400

    Add a labels selector

--- cmd/hrmm.go
+++ cmd/hrmm.go
@@ -10,6 +10,7 @@ import (
 var (
 	urls         []string
 	metrics      []string
+	labels       []string
 	jsonOutput   bool
 	pollInterval time.Duration
 )
@@ -52,8 +53,9 @@ var printCmd = &cobra.Command{
 
 func init() {
 	RootCmd.PersistentFlags().StringSliceVarP(&urls, "url", "u", []string{}, "URL of a prometheus metrics endpoint (required, can be repeated)")
-	RootCmd.PersistentFlags().StringSliceVarP(&metrics, "metric", "m", []string{}, "Name of a prometheus timeseries metric to graph (optional, can be repeated)")
-	RootCmd.PersistentFlags().DurationVarP(&pollInterval, "poll-interval", "p", 10*time.Second, "Poll interval for metrics collection (e.g., 10s, 1m, 500ms)")
+	RootCmd.PersistentFlags().StringSliceVarP(&metrics, "metric", "m", []string{}, "Select this prometheus metric name")
+	RootCmd.PersistentFlags().StringSliceVarP(&labels, "labels", "l", []string{}, "Select this Prometheus metric label")
+	RootCmd.PersistentFlags().DurationVarP(&pollInterval, "interval", "i", 10*time.Second, "Poll interval for metrics collection (e.g., 10s, 1m, 500ms)")
 	RootCmd.MarkPersistentFlagRequired("url")
 
 	printCmd.Flags().BoolVarP(&jsonOutput, "json", "j", false, "Output in JSON format")
